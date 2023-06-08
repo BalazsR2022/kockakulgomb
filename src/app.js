7e3c378d7328eddc1cbb36132735b5c995e188a3
Balázs Réka <balazsreka@ktch.hu>
2023-06-08 06:22:49 +0200

    formázás, mezők elkezdve

---
+++ src/app.js
@@ -0,0 +1,30 @@
+//File: index.html
+//Author: Balázs Réka
+//Copyright: 2023, Balázs Réka
+//Group: Szoft 1-2-E
+//Date: 2023-06-08
+//Github: https://github.com/BalazsR2022/Gombter
+
+const gomb = document.querySelector('button[value="atmero"]');
+gomb.addEventListener('click', szamol);
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+});
+
+  function szamolSugar(a) {
+    const pi = Math.PI;
+    return     R = (sqrt(3)*a)/2
+  }
+ 
+
+  function szamol() {
+    const a = document.querySelector('#megadottertek');
+    const R = Number 'Sugar' (sugar.value);
+ 
+    const sugarInput = document.querySelector('#gombsugar');
+    terfogatInput.value = terfogat;
+ 
+  }
+  
